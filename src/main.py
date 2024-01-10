c9413c46f82121ea28dadd36bf050d1a9fa851b7
champion254 <84034909+champion254@users.noreply.github.com>
2024-01-10 21:02:27 +0100

    oprava main

--- src/main.py
+++ src/main.py
@@ -9,23 +9,32 @@ def main1():
     a = list(a)
     print(a)
     print(len(a))
+
+
 def main2():
-    G = Graph(n=5, edges=[[1,0], [1, 2],[3,2],[1,4],[0,3],[2,4],[4,3]],directed = True)
+    G = Graph(n=5, edges=[[1, 0], [1, 2], [3, 2], [1, 4], [0, 3], [2, 4], [4, 3]], directed=True)
     print(create_matrix(G))
     matrix = create_matrix(G)
-    result = graphs_flow(VectorR.generate_vector_R(-2, 2, 5),matrix, VectorR.comparator_equal, VectorR.comparator_lesser, VectorR.comparator_bigger,
-                         VectorR.adition, VectorR.difference, VectorR.neutral_element,good_element)
+    result = graphs_flow(VectorR.generate_vector_R(-2, 2, 5), matrix, VectorR.comparator_equal,
+                         VectorR.comparator_lesser, VectorR.comparator_bigger,
+                         VectorR.adition, VectorR.difference, VectorR.neutral_element, good_element)
     print(result)
+
+
 def main3():
+    a = VectorR.generate_vector_R(-2, 2, 5)
+    a = list(a)
     G = Graph(n=2, edges=[[0, 1], [1, 0]], directed=True)
     result = graph_flows_brute(G, a, VectorR.comparator_equal, VectorR.comparator_lesser,
-                                     VectorR.comparator_bigger, VectorR.adition, VectorR.neutral_element)
+                               VectorR.comparator_bigger, VectorR.adition, VectorR.neutral_element)
     print(result)
+
 
 def good_element(vector):
     if VectorR.if_greater_than_one(*vector) and VectorR.if_lesser_than_x(*vector, 5):
         return True
     return False
+
 
 if __name__ == '__main__':
     main1()
